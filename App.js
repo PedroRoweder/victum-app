90363cc4619d5c7d438da21ad8fce0971b740d70
pedroroweder <pedrohroweder@hotmail.com>
2020-09-04 16:07:56 -0300

    Restructure the app for react-navigation

--- App.js
+++ App.js
@@ -1,37 +1,23 @@
 import React from "react";
 import { Provider } from "react-native-paper";
-import { StatusBar } from "expo-status-bar";
-import { MainButtonList } from "./components/MainButtonList";
-import { StyleSheet, View, Image } from "react-native";
-import img from "./images/autopflege_vetor-2.png";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { MainMenu } from "./view/MainMenu";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider>
-      <View style={styles.container}>
-        <Image source={img} style={styles.header}></Image>
-        <MainButtonList />
-        <StatusBar visible="auto" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="home" component={MainMenu} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FBFBFB",
-  },
-  header: {
-    marginTop: 100,
-    height: 250,
-    width: "96%",
-    marginHorizontal: "2%",
-    resizeMode: "contain",
-  },
-  title: {
-    fontSize: 50,
-    color: "black",
-    position: "absolute",
-  },
-});
---
+++ view/MainMenu.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { MainButtonList } from "./MainButtonList";
+import { StyleSheet, View, Image, Text } from "react-native";
+import img from "../images/autopflege_vetor-2.png";
+
+export const MainMenu = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={img} style={styles.header}></Image>
+      <MainButtonList />
+      <StatusBar visible="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FBFBFB",
+  },
+  header: {
+    marginTop: 100,
+    height: 250,
+    width: "96%",
+    marginHorizontal: "2%",
+    resizeMode: "contain",
+  },
+});
