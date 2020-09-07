2229b9b0e5f6833b428cc8f57e022be987271ab2
unknown <samuel_cxc@hotmail.com>
2020-09-07 18:39:36 -0300

    Version and copyright added to the main menu

--- view/MainMenu.js
+++ view/MainMenu.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { MainButtonList } from "./components/MainButtonList";
-import { StyleSheet, View, Image, Text, ActivityIndicator } from "react-native";
+import { StyleSheet, View, Image, Text, Linking } from "react-native";
 import img from "../images/autopflege_vetor-2.png";
 
 export const MainMenu = ({ navigation }) => {
@@ -12,6 +12,10 @@ export const MainMenu = ({ navigation }) => {
       <StatusBar visible="auto" />
       <View style={styles.footer}>
         <Text>v0.0.1a</Text>
+        <Text>
+          Copyright &copy; 2020 by{" "}
+          <Text style={{ fontWeight: "bold" }}>Sicario Programming</Text>
+        </Text>
       </View>
     </View>
   );
@@ -30,8 +34,9 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   footer: {
-    display: "flex",
-    alignItems: "flex-end",
-    marginRight: "8.5%",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginBottom: 1,
   },
 });
